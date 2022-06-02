f3d9306362f062890f72f05707635589c56aec90
KimSoobin32 <76720785+KimSoobin32@users.noreply.github.com>
2022-06-02 16:28:24 +0900

    Add files via upload

--- Array1/src/Array11.java
+++ Array1/src/Array11.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Array11 {
 
@@ -12,6 +13,21 @@ public class Array11 {
 		 * 결과 배열에 따른 조건은 60점 이상(포함) 중 합격자만 출력
 		 * 결과배열[hong, kim, lee, soun]
 		 */
+		//풀이는 금요일날..
+		String a[] = {"hong", "kim", "park", "jang", "lee", "soun"};
+		int b[] = {80, 100, 46, 38, 65, 88};
+		String na[] = new String[6];
+		int w = 0;
+		int wl = a.length;
+		int ct = 0;
+		while(w<wl) {
+			if(b[w]>=60) {
+				na[ct] = a[w];
+				ct++;
+			}
+			w++;
+		};
+		System.out.println(Arrays.toString(na));
 
 	}
 
